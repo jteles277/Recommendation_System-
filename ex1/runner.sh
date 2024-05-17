8a5ad7c3e9c1283f225e569fc42d207449ed8e3d
jteles277 <jteles277@gmail.com>
2024-05-18 00:10:44 +0100

    Clean up

--- ex1/runner.sh
+++ ex1/runner.sh
@@ -1,1 +1,4 @@
-spark-submit bfr.py fma_metadata/tracks.csv fma_metadata/features.csv results 
+spark-submit src/BFR.py fma_metadata/tracks.csv fma_metadata/features.csv results 
+spark-submit src/find_best_k.py fma_metadata/tracks.csv fma_metadata/features.csv results 
+
+spark-submit src/BFR.py fma_metadata/tracks.csv fma_metadata/features.csv results 
--- ex1/runner_teles.sh
+++
@@ -1,1 +0,0 @@
-spark-submit bfr_teles.py fma_metadata/tracks.csv fma_metadata/features.csv results 
